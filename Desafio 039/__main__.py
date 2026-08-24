c8a7a821e17dac5687a92a530209fac3ea381e54
Aleyxom <aleyxom.cs.oliveira@gmail.com>
2026-08-24 09:44:35 -0300

    desafio39 feito com re

--- Desafio 039/__main__.py
+++ Desafio 039/__main__.py
@@ -1,7 +1,7 @@
 from classes import *
 
 def main():
-    validar_dado(Usuario(), "Aleyxom-123")
+    validar_dado(Email(), "u@a.com")
 
 if __name__ == "__main__":
     main()
--- Desafio 039/classes.py
+++ Desafio 039/classes.py
@@ -2,36 +2,32 @@ from abc import ABC, abstractmethod
 import re
 
 class Validador(ABC):
-    def __init__(self):
-        pass
-
     @abstractmethod
     def validar(self, dado):
         pass
 
 class Usuario(Validador):
-    def __init__(self):
-        super().__init__()
-
     def validar(self, dado):
+        padrao = r"[A-Za-z0-9]"
         if (5 <= len(dado) <= 20) and (not re.search(r"\s", dado)) and (not re.search(r"[^\w]", dado)):
             return True
         else:
             return False
 
 class Email(Validador):
-    def __init__(self):
-        super().__init__()
-        pass
     def validar(self, dado):
-        pass
+        padrao = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+        if re.fullmatch(padrao, dado):
+            return True
+        else:
+            return False
 
 class Senha(Validador):
-    def __init__(self):
-        super().__init__()
-        pass
     def validar(self, dado):
-        pass
+        if (len(dado) >= 8) and (re.search(r"[^\w]", dado)) and (re.search(r"[A-Z]", dado )):
+            return True
+        else:
+            return False
 
 # DUCK TYPING -----------------------------------------
 def validar_dado(objeto, dado):
